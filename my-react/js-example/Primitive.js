f1719390eb2a9f393c00fb138cf589a2c8bcd34e
yeen322 <yemonmon321@gmail.com>
2025-03-28 08:57:39 +0900

    Primitive type

--- my-react/js-example/Primitive.js
+++ my-react/js-example/Primitive.js
@@ -1,16 +1,14 @@
 // 원시 타입 primitive type
-let a = 100;
-a =50;
-
-console.log(a);
-
+// let a = 100;
+// a =50;
 //
+// console.log(a);
+
+// 원시타입의 객체 복사
 let b = 20;
 let c = b;
 b=100;
 
 console.log(b); // 100
 console.log(c); // 20
-
-// 래퍼 객체 wrapper object
 
