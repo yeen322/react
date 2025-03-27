142a9e7ac45a73ee2cc71cff3c896fd6db741283
yeen322 <yemonmon321@gmail.com>
2025-03-28 08:58:45 +0900

    Reference(object) type

--- my-react/js-example/Reference.js
+++ my-react/js-example/Reference.js
@@ -7,3 +7,12 @@ const b = a;
 
 a.age = 23;
 console.log(b.age); // 23
+
+//얕은 복사
+let obj = {value: {num:10}};
+// let obj2 = {value: {num:10}};
+// let obj2 = obj;
+let obj2 = {value: obj.value};
+
+console.log(obj === obj2);
+console.log(obj.value === obj2.value);
