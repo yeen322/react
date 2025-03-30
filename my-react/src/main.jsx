e84fefb968436261d06e2301f4d1dc1432f5a126
yeen322 <yemonmon321@gmail.com>
2025-03-31 01:51:23 +0900

    main.jsx

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -1,38 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import MyHello from "./MyHello";
-import EventBaisc from "./EventBasic.jsx";
-import ForList from "../js-example/ForList.jsx";
+import StyledPanel from "../2-js-example/3/StyledPanel.jsx";
+import TitledPanel from "../2-js-example/3/TitledPanel.jsx";
+import ListTemplate from "../2-js-example/3/ListTemplate.jsx";
 import books from "../js-example/books.jsx";
-import ForFilter from "../js-example/ForFilter.jsx";
-import ForSort from "../js-example/ForSort.jsx";
-import StateBasic from "../js-example/StateBasic.jsx";
-import {Form} from "../js-example/Form.jsx";
-import Counter from "../js-example/CounterButton.jsx";
+import MyHello from "../2-js-example/3/MyHello.jsx";
 
 
-
+// const title= <p>회원 모집 중</p>
+// const body = <p>리액트 세미나에 오신 것을 환영합니다</p>
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        {/*<App />*/}
-        {/*<MyHello myName="Yeeun"/>*/}
-        {/*<EventBaisc type="time"/>*/}
-        {/*<ForList src ={books}/>*/}
-        {/*<ForFilter src={books}/>*/}
-        {/*<ForSort src={books}/>*/}
-        {/*<StateBasic/>*/}
-        {/*<Form/>*/}
-        <Counter></Counter>
+    {/*<ListTemplate src={books}>*/}
+    {/*    {elem => (*/}
+    {/*        <>*/}
+    {/*        <dt>*/}
+    {/*            <a href={`https://wikibook.co.kr/images/cover/s/${elem.isbn}.jpg`}>*/}
+    {/*                {elem.title} ({elem.price}원)*/}
+    {/*            </a>*/}
+    {/*        </dt>*/}
+    {/*        <dd>{elem.summary}< /dd>*/}
+    {/*            </>*/}
+    {/*            )}*/}
+    {/*        </ListTemplate>*/}
+    <MyHello />
+    </StrictMode>
 
-    </StrictMode>,
+
 )
-
-
-
-
-
 // const user = {
 //     name: "yeeun",
 //     age: 22,
