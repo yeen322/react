c1ee376ecb838e3f09dc81ddcd67f8673606922b
Yeeun Park <yemonmon321@gmail.com>
2025-04-01 16:03:48 +0900

    main.jsx

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -2,12 +2,15 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import EventKey from "../2-js-example/3/EventKey.jsx";
+import EventArgs from "../2-js-example/3/EventArgs.jsx";
+import EventArgs2 from "../2-js-example/3/EventArgs2.jsx";
 
 
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <EventKey/>
+        {/*<EventKey/>*/}
+        <EventArgs2/>
     </StrictMode>
 
 )
