8fa2b6fcdd7766787db0a228b12e5c6cc1245b12
Yeeun Park <yemonmon321@gmail.com>
2025-04-01 15:37:04 +0900

    main.jsx

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -1,29 +1,13 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import StyledPanel from "../2-js-example/3/StyledPanel.jsx";
-import TitledPanel from "../2-js-example/3/TitledPanel.jsx";
-import ListTemplate from "../2-js-example/3/ListTemplate.jsx";
-import books from "../js-example/books.jsx";
-import MyHello from "../2-js-example/3/MyHello.jsx";
-import StateParent from "../2-js-example/3/StateParent.jsx";
-import EventMouse from "../2-js-example/3/EventMouse.jsx";
-import EventError from "../2-js-example/3/EventError.jsx";
+import EventKey from "../2-js-example/3/EventKey.jsx";
 
 
-// const title= <p>회원 모집 중</p>
-// const body = <p>리액트 세미나에 오신 것을 환영합니다</p>
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        {/*<EventMouse*/}
-        {/*    alt="로고 이미지"*/}
-        {/*    beforeSrc="https://wwww.web-deli.com/image/linkbanner_l.gif"*/}
-        {/*    afterSrc="https://www.web-deli.com/image/home_chara.gif" />*/}
-
-        <EventError src="./image/wings.ipg"
-                    alt="샘플 이미지"/>
-
+        <EventKey/>
     </StrictMode>
 
 )
