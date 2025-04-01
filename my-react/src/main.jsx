0ed4684098707dc354419523a5dd01676da459ef
Yeeun Park <yemonmon321@gmail.com>
2025-04-01 17:27:49 +0900

    main.jsx

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -4,13 +4,17 @@ import './index.css'
 import EventKey from "../2-js-example/3/EventKey.jsx";
 import EventArgs from "../2-js-example/3/EventArgs.jsx";
 import EventArgs2 from "../2-js-example/3/EventArgs2.jsx";
+import StateParent from "../2-js-example/3/StateParent.jsx";
+import StateBasic from "../js-example/StateBasic.jsx";
 
 
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         {/*<EventKey/>*/}
-        <EventArgs2/>
+        {/*<EventArgs2/>*/}
+        {/*<StateParent/>*/}
+        <StateBasic/>
     </StrictMode>
 
 )
