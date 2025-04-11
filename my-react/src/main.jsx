8ebcecd185c8f2f0304abbf8c6cca2fbd2e5a040
yeen322 <yemonmon321@gmail.com>
2025-04-11 17:56:31 +0900

    main

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -2,10 +2,18 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import StateFormUC from "../2-js-example/4/SateFormUC.jsx";
+import FormTextarea from "../2-js-example/4/FormTextarea.jsx";
+import FormSelect from "../2-js-example/4/FormSelect.jsx";
+import FormList from "../2-js-example/4/FormList.jsx";
+import FormRadio from "../2-js-example/4/FormRadio.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <StateFormUC/>
+        {/*<StateFormUC/>*/}
+        {/*<FormTextarea/>*/}
+        {/*<FormSelect/>*/}
+        {/*<FormList/>*/}
+        <FormRadio/>
     </StrictMode>
 
 )
